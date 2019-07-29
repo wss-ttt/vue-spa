c6dcb74378b03246b22ac3e127dd8a137d0d54b7
王水胜 <602466011@qq.com>
2019-07-29 17:41:25 +0800

    更新:单页面应用切换

--- src/router/index.js
+++ src/router/index.js
@@ -4,42 +4,35 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-  	{
-  		path:'/',
-  		redirect: '/home'
-  	},
-    {
-    	path:'/',
-    	name:'main',
-    	component: resolve => require(['@/views/main.vue'], resolve),
-    	children:[
-    		{
-    			path: '/home',
-    			name:'home',
-          component: resolve => require(['@/components/home.vue'], resolve),
-    		},
-    		{
-    			path: '/blog',
-    			name:'blog',
-          component: resolve => require(['@/components/blog.vue'], resolve),
-    		},
-    		{
-    			path: '/news',
-    			name:'news',
-          component: resolve => require(['@/components/news.vue'], resolve),
-    		},
-    		{
-    			path: '/photo',
-    			name:'photo',
-          component: resolve => require(['@/components/photo.vue'], resolve),
-    		},
-    	]
-    },
-    {
-    	path:'/index',
-    	name:'index',
-      component: resolve => require(['@/views/index.vue'], resolve),
-    }
-  ]
+	routes: [
+	{
+		path:'/',
+		redirect:'/home'
+	},
+	{
+		path: '/',
+		name: 'index',
+		component: resolve => require(['@/views/index.vue'], resolve),
+		children: [{
+				path: '/home',
+				name: 'home',
+				component: resolve => require(['@/components/home.vue'], resolve),
+			},
+			{
+				path: '/blog',
+				name: 'blog',
+				component: resolve => require(['@/components/blog.vue'], resolve),
+			},
+			{
+				path: '/news',
+				name: 'news',
+				component: resolve => require(['@/components/news.vue'], resolve),
+			},
+			{
+				path: '/photo',
+				name: 'photo',
+				component: resolve => require(['@/components/photo.vue'], resolve),
+			},
+		]
+	}]
 })
